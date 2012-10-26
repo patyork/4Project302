83be84fb0dcea1745088ffb99a40ab5280af7678
Patrick <pat@ubuntu.(none)>
2012-10-25 19:31:22 -0700

    new file:   prog4.cpp

---
+++ prog4.cpp
@@ -0,0 +1,15 @@
+#include "simulation.h"
+
+int main()
+{
+	// Create a new simulation
+	simulation sim;
+	
+	// Initialize the simulation
+	sim.init();
+	
+	// Run the simulation
+	sim.run();
+	
+	return 0;
+}
